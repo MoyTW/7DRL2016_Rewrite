c970d98a42200516c98c6aea39fdb0256e040d6b
MoyTW <moytravis@gmail.com>
2016-07-04 13:46:52 -0700

    Add actor component
    Holds the speed/time_until_turn stats.

---
+++ dodge/components/actor.py
@@ -0,0 +1,25 @@
+from component import Component
+
+
+ActorName = 'ATTACKER'
+
+
+class Actor(Component):
+    def __init__(self, base_speed):
+        super(Actor, self).__init__(name=ActorName)
+
+        self.base_speed = base_speed
+        self._time_until_turn = self.base_speed
+
+    @property
+    def speed(self):
+        # buffs = sum(buff[0] for buff in self._speed_buffs)
+        # return self.base_speed - buffs
+        raise NotImplementedError()
+
+    @property
+    def time_until_turn(self):
+        return self._time_until_turn
+
+    def pass_time(self, time):
+        raise NotImplementedError()
