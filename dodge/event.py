71f74598029fbd47cfafa50520780773a6f800af
MoyTW <moytravis@gmail.com>
2016-07-10 14:42:54 -0700

    Check events params on construction

--- dodge/event.py
+++ dodge/event.py
@@ -23,8 +23,13 @@ class Event(object):
     def __init__(self, event_type, params):
         self.event_type = event_type
         self.params = params
+        if not self.is_event_type(event_type):
+            raise ValueError(str(event_type) + ' is missing required parameters! See: ' + str(params))
 
     def is_event_type(self, event_type):
+        if event_type not in event_templates:
+            raise ValueError('The desired event_type ' + str(event_type) + ' is not defined in the event templates!')
+
         for (k, required) in event_templates.get(event_type):
             if required and k not in self.params:
                 return False
--- tests/components/test_component.py
+++ tests/components/test_component.py
@@ -1,15 +1,15 @@
 import unittest
 from dodge.components.component import Component
-from dodge.event import Event
+from dodge.event import Event, EventType, EventParam
 
 
 class TestComponent(unittest.TestCase):
     def setUp(self):
-        self.c = Component('blue', [0])
+        self.c = Component('damage test', [EventType.DAMAGE])
 
     def tests_runs_fn_if_listening_for_event(self):
         with self.assertRaises(NotImplementedError):
-            self.c.handle_event(Event(0, {}))
+            self.c.handle_event(Event(EventType.DAMAGE, {EventParam.QUANTITY: 5}))
 
     def tests_returns_false_if_not_listening_for_event(self):
-        self.assertFalse(self.c.handle_event(Event(3, {})))
+        self.assertFalse(self.c.handle_event(Event(EventType.ATTACK, {EventParam.QUANTITY: 5})))
