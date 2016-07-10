41d207f03a5f4ce7cd824f780ea95b6f4cad4aea
MoyTW <moytravis@gmail.com>
2016-07-10 13:26:11 -0700

    Add config class

---
+++ dodge/config.py
@@ -0,0 +1,9 @@
+class Config(object):
+    def __init__(self, file_location):
+        self.SCREEN_WIDTH = 80
+        self.SCREEN_HEIGHT = 50
+        self.MAP_WIDTH = 300
+        self.MAP_HEIGHT = 300
+        self.PANEL_HEIGHT = 7
+        self.FULL_SCREEN = True
+        # etc etc etc
