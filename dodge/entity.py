be4607855d7f489503cadebd0b2fad95e66ae3e5
MoyTW <moytravis@gmail.com>
2016-07-11 19:43:08 -0700

    Add Entity class

---
+++ dodge/entity.py
@@ -0,0 +1,12 @@
+class Entity(object):
+    def __init__(self, eid, name, components=None):
+        self.eid = eid
+        self.name = name
+        self.components = {}
+        if components:
+            for component in components:
+                self.components[component.name] = component
+
+    def handle_event(self, event):
+        for component in self.components.values():
+            component.handle_event(event)
