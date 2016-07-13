383666fdc68a713216bdae1259f5ae8196f12b62
MoyTW <moytravis@gmail.com>
2016-07-12 18:34:38 -0700

    Complete Renderable component

--- dodge/components/renderable.py
+++ dodge/components/renderable.py
@@ -1,15 +1,14 @@
-from component import Component
-
-
-RenderableName = 'RENDERABLE'
+from component import Component, ComponentType
 
 
 class Renderable(Component):
     def __init__(self, char, color, always_visible=False):
-        super(Renderable, self).__init__(name=RenderableName)
+        super(Renderable, self).__init__(component_type=ComponentType.RENDERABLE,
+                                         target_events=[],
+                                         emittable_events=[])
         self.char = char
         self.color = color
         self.always_visible = always_visible
 
-    def handle_event(self, event):
+    def _handle_event(self, event):
         raise NotImplementedError()
