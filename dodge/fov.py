1ad6da7f4548706475451372c2a727d844035344
MoyTW <moytravis@gmail.com>
2016-07-13 18:50:16 -0700

    Forgot return in fov wrapper
    Thinking lisp-style, I guess!

--- dodge/fov.py
+++ dodge/fov.py
@@ -2,7 +2,7 @@ import libtcodpy as libtcod
 
 
 def create_fov_map(width, height):
-    libtcod.map_new(width, height)
+    return libtcod.map_new(width, height)
 
 
 def set_fov_tile_properties(fov_map, x, y, isTransparent, isWalkable):
