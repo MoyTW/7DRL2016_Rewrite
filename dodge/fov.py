87ea8608d5e0058c4964d7bae7e9de222aaaf616
MoyTW <moytravis@gmail.com>
2016-07-13 18:29:59 -0700

    Wrap libtcod fov
    Going to wrap all non-ui libtcod functionality

---
+++ dodge/fov.py
@@ -0,0 +1,9 @@
+import libtcodpy as libtcod
+
+
+def create_fov_map(width, height):
+    libtcod.map_new(width, height)
+
+
+def set_fov_tile_properties(fov_map, x, y, isTransparent, isWalkable):
+    libtcod.map_set_properties(fov_map, x, y, isTransparent, isWalkable)
