73dbf268e96ddcb4e0f1bce8f3c6d70634cb936f
MoyTW <moytravis@gmail.com>
2016-07-15 19:36:04 -0700

    Add tests for Actor
    Tests fail due to the enum comparison not working. Hmm. I thought
    that was supposed to be handled by the Enum class!?

---
+++ tests/components/test_actor.py
@@ -0,0 +1,24 @@
+import unittest
+from components import Actor
+from dodge.event import Event, EventType, EventParam
+
+
+class TestActorComponent(unittest.TestCase):
+    def setUp(self):
+        self.component = Actor(100)
+
+    def test_handles_pass_time(self):
+        event = Event(EventType.PASS_TIME, {EventParam.QUANTITY: 50})
+        self.assertTrue(self.component.handle_event(event))
+        self.assertEqual(50, self.component.ttl)
+
+    def test_errors_on_impossible_pass_time(self):
+        event = Event(EventType.PASS_TIME, {EventParam.QUANTITY: 150})
+        with self.assertRaises(ValueError):
+            self.component.handle_event(event)
+
+    def test_handles_end_turn(self):
+        self.component.handle_event(Event(EventType.PASS_TIME, {EventParam.QUANTITY: 50}))
+        end_turn_event = Event(EventType.END_TURN, {})
+        self.assertTrue(self.component.handle_event(end_turn_event))
+        self.assertEqual(100, self.component.ttl)
