ea0f377f842dadb9d689de8535e1144a78c00ceb
MoyTW <moytravis@gmail.com>
2016-07-17 16:56:22 -0700

    Allow Event to bypass templates constants
    Might want to make a factory or some other way of handling that
    particular templating issue, but for not, just defaults and can
    be overridden if needed.

--- dodge/event.py
+++ dodge/event.py
@@ -2,18 +2,22 @@ from dodge.constants import event_templates
 
 
 class Event(object):
-    def __init__(self, event_type, params):
+    def __init__(self, event_type, params, templates=event_templates):
         self.event_type = event_type
         self.params = params
-        if not self.is_event_type(event_type):
+        self.templates = templates
+        if templates is not None and not self.is_event_type(event_type):
             raise ValueError(str(event_type) + ' is missing required parameters! Has: ' + str(params) + ' Needs: ' +
-                             str(event_templates[event_type]))
+                             str(templates[event_type]))
 
     def is_event_type(self, event_type):
-        if event_type not in event_templates:
+        if self.templates is None:
+            raise ValueError('Cannot check event against templates when no templates have been provided!')
+
+        if event_type not in self.templates:
             raise ValueError('The desired event_type ' + str(event_type) + ' is not defined in the event templates!')
 
-        for (k, required) in event_templates.get(event_type):
+        for (k, required) in self.templates.get(event_type):
             if required and k not in self.params:
                 return False
         return True
