d4e817284c6d7ecf99c7647669c8606937626373
MoyTW <moytravis@gmail.com>
2016-07-20 20:04:20 -0700

    Add a step_towards pathfinding function
    It recalculates every turn, which is fine given that I expect the
    target position to be the player, who should really move every
    turn. However, if we have some static pathfinding maybe it should
    not continually recalculate in the future.

--- dodge/fov.py
+++ dodge/fov.py
@@ -13,3 +13,10 @@ class FOVMap(object):
 
     def in_fov(self, x, y):
         return libtcod.map_is_in_fov(self.fov_map, x, y)
+
+    def step_towards(self, x, y, target_x, target_y):
+        """Returns a single step from (x, y) to (target_x, target_y)"""
+        path = libtcod.path_new_using_map(self.fov_map)
+        libtcod.path_compute(path, x, y, target_x, target_y)
+        (t_x, t_y) = libtcod.path_walk(path, False)
+        return t_x - x, t_y - y
