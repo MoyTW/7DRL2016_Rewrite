31ab4282f3d8137e01480c85b8a5230138063d73
MoyTW <moytravis@gmail.com>
2016-07-26 18:51:26 -0700

    Add base path

---
+++ dodge/paths.py
@@ -0,0 +1,31 @@
+class Path:
+    def __init__(self, start_x, start_y):
+        self.start_x = start_x
+        self.start_y = start_y
+        self.current_step = 0
+        self._path = [(start_x, start_y)]
+
+    def current_position(self):
+        return self._path[self.current_step]
+
+    def _calc_step(self):
+        raise NotImplementedError()
+
+    def _calc(self, steps=1):
+        for _ in range(steps):
+            self._path.append(self._calc_step())
+
+    def step(self, steps=1):
+        if steps < 0:
+            raise ValueError('Cannot move along a path by a negative number!')
+
+        self.current_step += steps
+        if len(self._path) <= self.current_step:
+            self._calc(self.current_step - len(self._path) + 1)
+
+    def project(self, steps):
+        if steps < 0:
+            raise ValueError('Cannot project a path by a negative number!')
+
+        if len(self._path) <= self.current_step + steps:
+            self._calc(steps - len(self._path) + 1)
---
+++ tests/test_paths.py
@@ -0,0 +1,41 @@
+import unittest
+from dodge.paths import Path
+
+
+class TestPath(Path):
+    def _calc_step(self):
+        return self._path[-1][0] + 1, self._path[-1][1] + 1
+
+
+class TestBasePath(unittest.TestCase):
+    def setUp(self):
+        self.path = TestPath(0, 0)
+
+    def test_step(self):
+        self.path.step()
+        self.assertEqual(len(self.path._path), 2)
+        self.assertEqual(1, self.path.current_step)
+        self.assertEqual((1, 1), self.path.current_position())
+        self.path.step(4)
+        self.assertEqual(len(self.path._path), 6)
+        self.assertEqual(5, self.path.current_step)
+        self.assertEqual((5, 5), self.path.current_position())
+
+    def test_step_rejects_negatives(self):
+        with self.assertRaises(ValueError):
+            self.path.step(-1)
+
+    def test_project(self):
+        self.path.project(3)
+        self.assertEqual(len(self.path._path), 4)
+        self.assertEqual(0, self.path.current_step)
+        self.assertEqual((0, 0), self.path.current_position())
+
+        self.path.step()
+        self.assertEqual(len(self.path._path), 4)
+        self.assertEqual(1, self.path.current_step)
+        self.assertEqual((1, 1), self.path.current_position())
+
+    def test_project_rejects_negatives(self):
+        with self.assertRaises(ValueError):
+            self.path.project(-2)
