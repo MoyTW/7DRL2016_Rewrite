e19c6aa0042a587e90790beee3b944dbd3904369
MoyTW <moytravis@gmail.com>
2016-08-15 18:56:36 -0700

    Add tests for Inventory
    Wow that's a lotta test code I just wrote.

--- dodge/components/inventory.py
+++ dodge/components/inventory.py
@@ -5,31 +5,55 @@ from dodge.constants import ComponentType, EventType, EventParam
 class Inventory(Component):
     """ A component which can hold other entities, if they are marked with the Carryable component. Will not
     re-broadcast any events to the items in your inventory as of this time. """
-    def __init__(self, event_stack, size):
+    def __init__(self, event_stack, max_size):
         super().__init__(ComponentType.INVENTORY,
                          target_events=[EventType.ADD_ITEM_TO_INVENTORY, EventType.REMOVE_ITEM_FROM_INVENTORY,
                                         EventType.PICK_UP_ITEM, EventType.DROP_ITEM],
                          emittable_events=[EventType.ADD_TO_LEVEL, EventType.REMOVE_FROM_LEVEL],
                          event_stack=event_stack)
         self._carried = []
-        self._size = size
+        self._max_size = max_size
 
     @property
     def size(self):
-        return self._size
+        return len(self._carried)
+
+    @property
+    def max_size(self):
+        return self._max_size
+
+    @property
+    def carried(self):
+        return self._carried
+
+    def _add_item(self, item):
+        raise NotImplementedError()
+
+    def _remove_item(self, item):
+        raise NotImplementedError()
+
+    def _remove_item_from_level(self, item):
+        raise NotImplementedError()
+
+    def _add_item_to_level(self, item, owner_pos):
+        raise NotImplementedError()
 
     def _handle_event(self, event):
+        owner_pos = event[EventParam.HANDLER].get_component(ComponentType.POSITION)
+        item = event[EventParam.ITEM]
         if event.event_type == EventType.ADD_ITEM_TO_INVENTORY:
-            raise NotImplementedError()
+            self._add_item(item)
             return True
         elif event.event_type == EventType.PICK_UP_ITEM:
-            raise NotImplementedError()
+            self._remove_item_from_level(item)
+            self._add_item(item)
             return True
         elif event.event_type == EventType.REMOVE_ITEM_FROM_INVENTORY:
-            raise NotImplementedError()
+            self._remove_item(item)
             return True
         elif event.event_type == EventType.DROP_ITEM:
-            raise NotImplementedError()
+            self._add_item_to_level(item, owner_pos)
+            self._remove_item(item)
             return True
         else:
             return False
---
+++ unittests/components/test_inventory.py
@@ -0,0 +1,98 @@
+import unittest
+from dodge.constants import EventType, EventParam, ComponentType
+from dodge.components import Inventory, Position
+from dodge.event import Event
+from dodge.entity import Entity
+from unittests.utils import EventStackStub, LevelStub
+
+
+class TestInventory(unittest.TestCase):
+    def setUp(self):
+        self.stack = EventStackStub()
+        self.max_size = 10
+        self.inventory = Inventory(self.stack, self.max_size)
+        self.item = Entity(0, 0, [])
+        self.handler = Entity(1, 1, [Position(self.stack, 3, 7, True)])
+
+    def test_construction(self):
+        self.assertEqual(10, self.inventory.max_size)
+        self.assertEqual(0, self.inventory.size)
+
+    def test_adds_directly_happy_case(self):
+        event = Event(EventType.ADD_ITEM_TO_INVENTORY, {EventParam.HANDLER: self.handler, EventParam.ITEM: self.item})
+        self.inventory.handle_event(event)
+        self.assertTrue(self.item in self.inventory.carried)
+        self.assertEqual(1, self.inventory.size)
+
+    def test_cannot_add_same_item_twice(self):
+        event = Event(EventType.ADD_ITEM_TO_INVENTORY, {EventParam.HANDLER: self.handler, EventParam.ITEM: self.item})
+        self.inventory.handle_event(event)
+
+        with self.assertRaises(ValueError):
+            self.inventory.handle_event(event)
+
+    def test_adds_directly_excepts_on_size_overflow(self):
+        for x in range(10):
+            item = Entity(x, x, None)
+            event = Event(EventType.ADD_ITEM_TO_INVENTORY, {EventParam.HANDLER: self.handler, EventParam.ITEM: item})
+            self.inventory.handle_event(event)
+        with self.assertRaises(ValueError):
+            item = Entity('fails', 'fails', None)
+            event = Event(EventType.ADD_ITEM_TO_INVENTORY, {EventParam.HANDLER: self.handler, EventParam.ITEM: item})
+            self.inventory.handle_event(event)
+
+    def test_removes(self):
+        self.inventory._add_item(self.item)
+        self.assertTrue(self.item in self.inventory.carried)
+
+        event = Event(EventType.REMOVE_ITEM_FROM_INVENTORY,
+                      {EventParam.HANDLER: self.handler, EventParam.ITEM: self.item})
+        self.inventory.handle_event(event)
+        self.assertTrue(self.item not in self.inventory.carried)
+        self.assertEqual(0, self.inventory.size)
+
+    def test_removes_excepts_if_item_not_there(self):
+        event = Event(EventType.REMOVE_ITEM_FROM_INVENTORY,
+                      {EventParam.HANDLER: self.handler, EventParam.ITEM: self.item})
+        with self.assertRaises(ValueError):
+            self.inventory.handle_event(event)
+
+    def test_picks_up_happy_case(self):
+        level = LevelStub(None, self.item)
+        event = Event(EventType.PICK_UP_ITEM, {EventParam.HANDLER: self.handler,
+                                               EventParam.ITEM: self.item,
+                                               EventParam.LEVEL: level})
+        self.inventory.handle_event(event)
+        self.assertTrue(self.item in self.inventory.carried)
+        self.assertEqual(1, self.inventory.size)
+
+        remove_event = self.stack.pop()  #type: Event
+        self.assertTrue(self.stack.is_empty())
+        self.assertEqual(remove_event.event_type == EventType.REMOVE_FROM_LEVEL)
+        self.assertEqual(self.item, remove_event[EventParam.TARGET])
+
+    def test_picks_up_excepts_if_item_not_same_position(self):
+        level = LevelStub(None, None)
+        event = Event(EventType.PICK_UP_ITEM, {EventParam.HANDLER: self.handler,
+                                               EventParam.ITEM: self.item,
+                                               EventParam.LEVEL: level})
+        with self.assertRaises(ValueError):
+            self.inventory.handle_event(event)
+
+    def test_drops_happy_case(self):
+        self.inventory._add_item(self.item)
+
+        level = LevelStub(None, None)
+        event = Event(EventType.DROP_ITEM, {EventParam.HANDLER: self.handler,
+                                            EventParam.ITEM: self.item,
+                                            EventParam.LEVEL: level})
+        self.inventory.handle_event(event)
+        self.assertEqual(0, self.inventory.size)
+
+        add_event = self.stack.pop()  # type: Event
+        self.assertEqual(0, len(self.stack.view()))
+        self.assertEqual(EventType.ADD_TO_LEVEL, add_event.event_type)
+        item_pos = self.item.get_component(ComponentType.POSITION)
+        self.assertEqual(3, item_pos.x)
+        self.assertEqual(7, item_pos.y)
+        self.assertEqual(False, item_pos.blocks)
